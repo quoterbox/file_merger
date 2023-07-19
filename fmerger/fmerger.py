cf8330e388a3401e115fb04563b433baf712a7a3
John Go <jooqeer@yandex.ru>
2023-07-19 15:55:41 +0200

    Fixed the reading files with a different encoding

--- fmerger/fmerger.py
+++ fmerger/fmerger.py
@@ -12,10 +12,10 @@ def create_dict_fields(row: {}) -> {}:
 
 
 class TableFile:
-    file_name = ""
+    _file_name = ""
 
     def __init__(self, file_name: ""):
-        self.file_name = file_name
+        self._file_name = file_name
 
     def get_file_first_line(self) -> []:
         return []
@@ -27,7 +27,7 @@ class TableFile:
 class XslxFile(TableFile):
 
     def get_file_first_line(self) -> []:
-        wb = load_workbook(filename=self.file_name)
+        wb = load_workbook(filename=self._file_name)
         ws = wb.active
         row = ""
 
@@ -37,7 +37,7 @@ class XslxFile(TableFile):
         return row
 
     def get_data_from_file(self) -> []:
-        wb = load_workbook(filename=self.file_name)
+        wb = load_workbook(filename=self._file_name)
         ws = wb.active
         xlsx_rows = []
         cnt = 0
@@ -51,31 +51,29 @@ class XslxFile(TableFile):
 
 
 class CsvFile(TableFile):
+    __encoding = ""
+    __delimiter = ";"
+    __newline = ""
 
-    delimiter = ";"
-    newline = ""
-
-    def __init__(self, file_name: str, delimiter: str, newline: str):
+    def __init__(self, file_name: str, delimiter: str, newline: str, **kwargs):
         super().__init__(file_name)
-        self.delimiter = delimiter
-        self.newline = newline
+        self.__delimiter = delimiter
+        self.__encoding = kwargs.get("encoding", "utf-8")
+        self.__newline = newline
 
     def get_file_first_line(self) -> []:
-        with open(self.file_name, "r", newline=self.newline) as doc:
-            reader = csv.DictReader(doc, delimiter=self.delimiter)
+        with open(self._file_name, "r", encoding=self.__encoding, newline=self.__newline) as doc:
+            reader = csv.DictReader(doc, delimiter=self.__delimiter)
             headers = reader.fieldnames
-
         return headers
 
     def get_data_from_file(self) -> []:
-
         csv_rows = []
-        with open(self.file_name, "r", newline=self.newline) as doc:
-            reader = csv.reader(doc, delimiter=self.delimiter)
+        with open(self._file_name, "r", encoding=self.__encoding, newline=self.__newline) as doc:
+            reader = csv.reader(doc, delimiter=self.__delimiter)
             next(reader, None)
             for row in reader:
                 csv_rows.append(row)
-
         return csv_rows
 
 
@@ -83,28 +81,30 @@ class FMerger:
     valid_files = [".csv", ".xlsx"]
     table_file = None
     source_dir = ""
-    source_encoding = ""
+    source_encoding = "utf-8"
     source_delimiter = ";"
     source_newline = ""
     target_file = "merged_files.csv"
-    target_encoding = ""
+    target_encoding = "utf-8"
     target_delimiter = ";"
     target_newline = ""
 
     def __init__(self, options: {}):
         self.source_dir = options["sources_options"]["source_dir"]
-        self.source_encoding = options["sources_options"]["source_encoding"]
+        if "source_encoding" in options.get("sources_options", {}):
+            self.source_encoding = options["sources_options"]["source_encoding"]
         self.source_delimiter = options["sources_options"]["source_delimiter"]
         self.source_newline = options["sources_options"]["source_newline"]
         self.target_file = options["target_file_options"]["target_file"]
-        self.target_encoding = options["target_file_options"]["target_encoding"]
+        if "target_encoding" in options.get("target_file_options", {}):
+            self.target_encoding = options["target_file_options"]["target_encoding"]
         self.target_delimiter = options["target_file_options"]["target_delimiter"]
         self.target_newline = options["target_file_options"]["target_newline"]
 
     def merge(self):
         cnt = 0
         for file in self.__read_directory_files(self.source_dir):
-            table_file = self.__create_table_file(file, self.source_delimiter, self.source_newline)
+            table_file = self.__create_table_file(file, self.source_delimiter, self.source_newline, self.source_encoding)
             if cnt == 0:
                 first_line = table_file.get_file_first_line()
                 self.__create_target_file(self.target_file, create_dict_fields(first_line))
@@ -114,12 +114,12 @@ class FMerger:
             cnt += 1
 
     def __create_target_file(self, file_name: str, line: {}):
-        with open(file_name, "a", newline=self.target_newline) as doc:
+        with open(file_name, "a", encoding=self.target_encoding, newline=self.target_newline) as doc:
             writer = csv.DictWriter(doc, delimiter=self.target_delimiter, fieldnames=line)
             writer.writerow(line)
 
     def __write_target_file(self, rows: []):
-        with open(self.target_file, "a", newline=self.target_newline) as tg_file:
+        with open(self.target_file, "a", encoding=self.target_encoding, newline=self.target_newline) as tg_file:
             writer = csv.writer(tg_file, delimiter=self.target_delimiter)
             for row in rows:
                 writer.writerow(row)
@@ -139,8 +139,10 @@ class FMerger:
         return files
 
     @staticmethod
-    def __create_table_file(file: str, delimiter: str, newline: str) -> TableFile:
-        if file.endswith(".xlsx"):
-            return XslxFile(file)
-        elif file.endswith(".csv"):
-            return CsvFile(file, delimiter, newline)
+    def __create_table_file(table_file_path: str, delimiter: str, newline: str, encoding: str) -> TableFile:
+        table = None
+        if table_file_path.endswith(".xlsx"):
+            table = XslxFile(table_file_path)
+        elif table_file_path.endswith(".csv") and delimiter:
+            table = CsvFile(table_file_path, delimiter, newline, encoding=encoding)
+        return table
--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from fmerger.fmerger import FMerger
 if __name__ == "__main__":
     file_merger = FMerger({
         "sources_options": {
-            "source_dir": "files_xlsx",
-            "source_encoding": "",
+            "source_dir": "files_csv",
+            # "source_encoding": "utf-8",
             "source_delimiter": ";",
             "source_newline": "",
         },
         "target_file_options": {
             "target_file": "merged_files.csv",
-            "target_encoding": "",
+            # "target_encoding": "",
             "target_delimiter": ";",
             "target_newline": "",
         }
